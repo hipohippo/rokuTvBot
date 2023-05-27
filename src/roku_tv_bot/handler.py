d971a040a0c1827f769708228a44b4ee0921bfdd
hipohippo <hipohippo@github>
2023-05-27 00:54:01 -0400

    fixed query answer

--- src/roku_tv_bot/handler.py
+++ src/roku_tv_bot/handler.py
@@ -30,29 +30,14 @@ async def execute_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query: CallbackQuery = update.callback_query  # button callback data
     tv_connected = check_tv_connection(context)
     if not tv_connected:
-        status_msg = "not connected"
+        status_msg = "TV not connected"
     elif query.data is None:
         status_msg = "invalid query"
     else:
         await roku_action(context.bot_data["roku"], query.data, update, context)
         status_msg = f"selected {query.data.upper()} at {pd.Timestamp.now().strftime('%T.%f')}"
+    await query.answer(text=status_msg)
 
-    response_message: Message = context.chat_data.get("response_message", None)
-    if response_message is None:
-        context.chat_data["response_message"] = await context.bot.send_message(
-            text=status_msg, chat_id=update.effective_chat.id
-        )
-    elif status_msg != response_message.text:
-        await context.bot.edit_message_text(
-            text=status_msg, chat_id=update.effective_chat.id, message_id=response_message.message_id
-        )
-        # await context.bot.edit_message_text(
-        #    text=status_msg,
-        #    chat_id=query.message.chat_id,
-        #    message_id=query.message.message_id,
-        #    reply_markup=bot_config.static_keyboard_markup,
-        # )
-    return
 
 def check_tv_connection(context: ContextTypes.DEFAULT_TYPE) -> bool:
     """
